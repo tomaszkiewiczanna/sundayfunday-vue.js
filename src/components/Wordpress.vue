1c3825993cf443c3112ca45f5b1c5e06ca64339b
Anna Tomaszkiewicz <tomaszkiewicz.ania@gmail.com>
2018-11-06 23:47:25 +0100

    articles fetch

--- src/components/Wordpress.vue
+++ src/components/Wordpress.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="wordpress">
     <h1 class="wordpress-head">{{msg}}</h1>
-    <h2 class="wordpress-head">Kava posts</h2>
+    <h2 class="wordpress-subhead">Kava posts</h2>
     <router-link to="/">Back</router-link>
     <ul class="wordpress-posts">
-      
+      <li v-for="post in posts" class="wordpress-post">
+        <h1 class="wordpress-posts-head">{{post.title.rendered}}</h1>
+        <span class="wordpress-posts-content" v-html="post.content.rendered"></span>
+      </li>
     </ul>
   </div>
 </template>
@@ -28,3 +31,21 @@
     }
   }
 </script>
+<style>
+  .wordpress-post{
+    display: block;
+    width: 70vw;
+    height: auto;
+    overflow: hidden;
+    background: #f2f2f2;
+    padding: 20px;
+    margin: 30px 0;
+  }
+  .wordpress-post span{
+    font-size: 12px;
+    color: #404040;
+  }
+  .wordpress-posts-head{
+    color: #3a827f;
+  }
+</style>
